769dc58953912ab886e9aff2c72ebbd001bafa2d
LIMWOOCHUL <kmstar72@naver.com>
2024-04-09 11:16:47 +0900

    24.04.09 수정사항 적용

--- application/www/customer/storeInformation.view.php
+++ application/www/customer/storeInformation.view.php
@@ -9,13 +9,17 @@ $storeId = $view->getParams(0);
 $customerModel = $view->import('model.mall.customer');
 $cityList = $customerModel->getCity();
 
+$store_code = "31008";
+
 if(!empty($storeId)) {
-    $storeInfo = $customerModel->getStoreList(1,10,"","","",$storeId);
-    $view->assign('storeName', $storeInfo['list'][0]['store_name']);
+	$storeinfo = $customerModel->getStoreInfo($storeId);
+	$store_code = $storeinfo[0]["store_code"];
+    //$storeInfo = $customerModel->getStoreList(1,10,"","","",$storeId);
+    //$view->assign('storeName', $storeInfo['list'][0]['store_name']);
 }
 $view->assign('cityList', $cityList);
 
-$info = $customerModel->getStoreInfo("31008");
+$info = $customerModel->getStoreInfo($store_code);
 $view->assign('storeBasicInfo', $info[0]);
 
 echo $view->loadLayout('customer');
--- application/www/customer/storeInformationDetail.view.php
+++ application/www/customer/storeInformationDetail.view.php
@@ -4,9 +4,14 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 $view = getForbizView();
 $param = $view->getParams(0);
 
+redirect("/customer/storeInformation/".$param);
+print_r($param);
+exit;
+/*
 $customerModel = $view->import('model.mall.customer');
 $info = $customerModel->getStoreInfo($param);
 
 $view->assign('storeInfo', $info[0]);
 
 echo $view->loadLayout();
+*/
