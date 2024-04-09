6f6da13c1b211a54a67aebdc0ec33d75f66a0b2b
LIMWOOCHUL <kmstar72@naver.com>
2024-04-09 16:47:23 +0900

    24.04.09 수정사항 적용

--- application/www/customer/storeInformationDetail.view.php
+++ application/www/customer/storeInformationDetail.view.php
@@ -4,9 +4,18 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 $view = getForbizView();
 $param = $view->getParams(0);
 
-redirect("/customer/storeInformation/".$param);
-print_r($param);
-exit;
+if (is_mobile()) {
+    $customerModel = $view->import('model.mall.customer');
+    $info = $customerModel->getStoreInfo($param);
+
+    $view->assign('storeInfo', $info[0]);
+
+    echo $view->loadLayout();
+}else{
+    redirect("/customer/storeInformation/".$param);
+    print_r($param);
+}
+
 /*
 $customerModel = $view->import('model.mall.customer');
 $info = $customerModel->getStoreInfo($param);
